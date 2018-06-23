f79880e22782d3018bc0bb54ba6ec1fa3f53b4fc
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2018-06-23 13:10:17 +0900

    切り上げの計算法を変えた

--- src/apple.c
+++ src/apple.c
@@ -8,8 +8,7 @@ int ok(int x) {
     int i;
     int cnt = 0;
     for (i = 0; i < n; i++) {
-        if (A[i] % x) cnt += A[i]/x + 1;
-        else cnt += A[i]/x;
+        cnt += (A[i] - 1) / x + 1;
         if (cnt > k) return 0;
     }
     return 1;
